8916c9d78c4e5d676ffefddcdd137b745d218960
ducanhptitb19cn028 <99241227+ducanhptitb19cn028@users.noreply.github.com>
2023-03-12 15:35:21 +0700

    init

--- src/main/java/group/g22/demostore/service/EmployeeServiceImpl.java
+++ src/main/java/group/g22/demostore/service/EmployeeServiceImpl.java
@@ -2,21 +2,20 @@ package group.g22.demostore.service;
 
 import group.g22.demostore.model.Employee;
 import group.g22.demostore.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
-    private final EmployeeRepository employeeRepository;
-
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
-
+    @Autowired
+    private EmployeeRepository employeeRepository;
     @Override
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
